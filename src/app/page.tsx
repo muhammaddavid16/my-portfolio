3aa91a0a1146c889f2898c911e43f3a46685255c
muhammaddavid16 <muhammaddavid1650@gmail.com>
2025-05-04 16:25:04 +0700

    fix single quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
         <main className="px-4">
             <div className="max-w-6xl h-full mx-auto grid place-items-center">
                 <div className="text-center">
-                    <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tight">Hello, I'm David</h1>
+                    <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tight">Hello, I&#39;m David</h1>
                     <div className="max-w-3xl mt-6">
                         <p className="leading-7">
-                            I'm a Quality Assurance Automation Engineer based in Jakarta with a background in Computer
-                            Science from Universitas Nusamandiri.
+                            I&#39;m a Quality Assurance Automation Engineer based in Jakarta with a background in
+                            Computer Science from Universitas Nusamandiri.
                         </p>
 
                         <Button className="mt-4 rounded-full" asChild>
